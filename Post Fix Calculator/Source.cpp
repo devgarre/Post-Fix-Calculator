1b1bfbb891e2a9a691d407bb4ef4e538009a7c07
KNOWN-UNIVERSE\devgarre <devgarre@uat.edu>
2018-07-19 12:04:50 -0700

    Add project files.

---
+++ Post Fix Calculator/Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+string calculation;
+string currentCharacter;
+int value1;
+int value2;
+int solution;
+stack <int> valueList;
+
+int main() {
+
+	cout << "Enter the expression you would like to calculate: ";
+	cin >> calculation;
+
+
+
+	if (currentCharacter == "*") {
+		value2 = valueList.top();
+		valueList.pop();
+		value1 = valueList.top();
+		valueList.pop();
+		solution = value1 * value2;
+		valueList.push(solution);
+	}
+	else if (currentCharacter == "/") {
+		value2 = valueList.top();
+		valueList.pop();
+		value1 = valueList.top();
+		valueList.pop();
+		solution = value1 / value2;
+		valueList.push(solution);
+	}
+	else if (currentCharacter == "+") {
+		value2 = valueList.top();
+		valueList.pop();
+		value1 = valueList.top();
+		valueList.pop();
+		solution = value1 + value2;
+		valueList.push(solution);
+	}
+	else if (currentCharacter == "-") {
+		value2 = valueList.top();
+		valueList.pop();
+		value1 = valueList.top();
+		valueList.pop();
+		solution = value1 - value2;
+		valueList.push(solution);
+	}
+	else if (currentCharacter == "%") {
+		value2 = valueList.top();
+		valueList.pop();
+		value1 = valueList.top();
+		valueList.pop();
+		solution = value1 % value2;
+		valueList.push(solution);
+	}
+	else {
+		int tempValue = stoi(currentCharacter);
+		valueList.push(tempValue);
+	}
+
+	system("pause");
+	return 0;
+}
